deacbca157fda5beae9c7588ab48a13515febc22
Alex Khawalid <a.khawalid@gmail.com>
2015-09-21 08:21:20 +0200

    Implemented routing
    Routing added, also implemented routing. The addition of routing broke
    the navigation toggle, currently still needs to be fixed

--- score-app/score-app.js
+++ score-app/score-app.js
@@ -1,22 +1,103 @@
 Tasks = new Mongo.Collection("tasks");
 
- 
+// routing
+Router.route('/', {
+    name: 'home',
+    template: 'home'
+});
+
+Router.route('/register', {
+    name: 'register',
+    template: 'register'
+});
+
+Router.route('/tournament-1', {
+    name: 'tournament-1',
+    template: 'field_view'
+});
+
+// client side code
 if (Meteor.isClient) {
+    console.log(Meteor.users.find().fetch());
+    // toggle sidebar
+    Template.body.events({
+        "click .side-bar": function (e) {
+            e.preventDefault();
+            var el = $('#wrapper');
+            if (!el.hasClass("open-sidebar")) {
+                el.addClass('open-sidebar')
+            } else {
+                el.removeClass('open-sidebar')
+            }
 
-  Template.body.events({
-    "click .side-bar": function (event) {
-      console.log($("#wrapper"));
-      var el = $('#wrapper');
-      if (!el.hasClass("open-sidebar")) {
-        el.addClass('open-sidebar')
-      } else {
-        el.removeClass('open-sidebar')
-      }
+        }
+    });
 
-    }
-  });
+    // if submit button of a form in register template is pressed
+    // create user account
+    Template.register.events({
+        'submit form': function(e) {
+            e.preventDefault();
+            var username = $('[name=username]').val();
+            var password = $('[name=password]').val();
+            Accounts.createUser({
+                username: username,
+                password: password
+            }, function (error) {
+                // if errors, return reason else go home
+                if (error) {
+                    console.log(error.reason);
+                    window.alert(error.reason);
+                } else {
+                    Router.go('home');
+                }
+            });
+        }
+    });
 
-  Accounts.ui.config({
-    passwordSignupFields: "USERNAME_ONLY"
-  });
+    // log out button in the sidebar
+    Template.sidebar.events({
+        'click .logout': function(e) {
+            e.preventDefault();
+            Meteor.logout();
+            Router.go('login');
+        }
+    });
+
+    // log in page
+    Template.login.events({
+        'submit form': function(e) {
+            e.preventDefault();
+            var username = $('[name=username]').val();
+            var password = $('[name=password]').val();
+            Meteor.loginWithPassword(username,password,function(err) {
+                // if errors, return reason else go home
+                if (error) {
+                    console.log(error.reason);
+                } else {
+                    Router.go('home');
+                }
+            });
+        },
+
+        // refer to registration
+        // fill in user info
+        'click button': function(e) {
+            e.preventDefault();
+            var username = $('[name=username]').val();
+            var password = $('[name=password]').val();
+            window.alert(username);
+            Router.go('/register');
+            $('[name=username]').val(username);
+            $('[name=password]').val(password);
+        }
+    });
+
+    Accounts.ui.config({
+        passwordSignupFields: "USERNAME_ONLY"
+    });
+}
+
+if (Meteor.isServer) {
+    Meteor.users.find().fetch();
 }

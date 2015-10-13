dc27a402b13d60a3a0b15628a04547208201ca4d
Max Wong <wong@live.nl>
2015-10-13 10:20:17 +0200

    dynamic linking werkt, vanaf hier verder werken

--- score-app/score-app.js
+++ score-app/score-app.js
@@ -1,24 +1,66 @@
+Tournaments = new Meteor.Collection('tournaments');
+
+// Routes
+Router.configure({
+    layoutTemplate: 'main'
+})
+Router.route('/', {
+    name: 'home',
+    template: 'homePage'
+});
+Router.route('/register', {
+    template: 'registerPage'
+});
+Router.route('/login', {
+    template: 'loginPage'
+});
+Router.route('/tournament/:id', {
+    name: 'tournamentPage',
+    template: 'tournamentPage',
+    data: function(){
+        // Soms moet het met parseInt worden gedaan om onbekende redenen
+        // Later testen met echte data om te kijken of het nog steeds voorkomt.
+        var currentTournament = this.params["id"];
+        var returnValue = Tournaments.findOne({id: currentTournament});
+        if (returnValue) {
+            return returnValue
+        }
+        else {
+            return Tournaments.findOne({id: parseInt(currentTournament)});
+        }
+    }
+});
+
 if (Meteor.isClient) {
-  
+    // Find all tournaments
+    Template.tournamentView.helpers({
+        'tournament': function(){
+            return Tournaments.find({}, {sort: {name: 1}});
+        }
+    })
+
+    // Count tournaments
+    Template.tournamentsCount.helpers({
+        'totalTournaments': function(){
+            return Tournaments.find().count();
+        }
+    })
+
+    // mag later weg
+    Template.addTournament.events({
+        'submit form': function(event){
+            event.preventDefault();
+            var tournamentName = $('[name=tournamentName]').val()
+            Tournaments.insert({name: tournamentName, 
+                id: Math.floor(Math.random() * 10000) + 1}, 
+                function(error, results){
+                    Router.go('tournamentPage', {_id: results});
+            });
+            $('[name=tournamentName]').val('');
+        }
+    })
 }
 
 if (Meteor.isServer) {
-  Meteor.methods({
-        updateTournament: function () {
-            this.unblock();
-            return Meteor.http.call("GET", "https://api.leaguevine.com/v1/tournaments/" + tid + "/");
-        }
-    });
 
-    //  update tournaments
-    Meteor.call("updateTournament", function(error,results) {
-
-        if (Tournaments.find({id: results.data["id"]}).count()==0)
-        {
-            Tournaments.insert(
-                { name: results.data["name"], id: results.data["id"]}
-            );
-        }
-
-    });
 }
